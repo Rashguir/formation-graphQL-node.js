9fde3849283a39c71ae09b33544cfa3434f6b240
Nicolas Sicard <rashguir@gmail.com>
2018-10-18 12:00:23 +0200

    1/4 avancement

--- index.js
+++ index.js
@@ -4,37 +4,71 @@ const { ApolloServer, gql } = require("apollo-server");
 const typeDefs = gql`
   type Query {
     hello(name: String): String
-    ideas: [Idea!]!
+    ideas(id: ID): [Idea!]!
   },
   type Idea {
     id: ID!
-    title: String
+    title: String!
     desc: String
-    vote: Int
+    vote: Int!
+  }
+  input IdeaInput {
+    title: String!
+    desc: String
+  }
+  type Mutation {
+    upVote(id: ID!): Idea!
+    insertIdea(input: IdeaInput!): Idea!
   }
 `;
+
+const ideas = [
+  {
+    id: 1,
+    title: "titre 1",
+    desc: "desc 1",
+    vote: 3
+  },
+  {
+    id: 2,
+    title: "title 2",
+    desc: "desc 2",
+    vote: 2
+  }
+];
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
     hello: (root, args, context) => "Hello " + args.name,
-    ideas: (_, args, context) => [
-      {
-        title: "titre 1",
-        desc: "desc 1",
-        vote: 3
-      },
-      {
-        title: "title 2",
-        desc: "desc 2",
-        vote: 2
+    ideas: (_, args, context) => {
+      if (args.id) {
+        return ideas.filter(idea => idea.id === Number(args.id));
       }
-    ]
+      return ideas;
+    }
   },
-  Idea: {
-    title: (root, args, context) => "Titre 1",
-    desc: (root, args, context) => "Dat desc omg!",
-    vote: (root, args, context) => 13
+  Mutation: {
+    upVote: (_, args, context) => {
+      let idea = ideas.filter(idea => idea.id === Number(args.id))[0];
+      // console.log(idea);
+      if (!idea) {
+        throw new Error("Idea not found!");
+      }
+      idea.vote += 1;
+
+      return idea;
+
+      // return {
+      //   ...idea,
+      //   vote: idea.vote + 1
+      // };
+    },
+    insertIdea: (_, args, context) => {
+      let ideaInput = args.input;
+
+      console.log(ideaInput);
+    }
   }
 };
 
